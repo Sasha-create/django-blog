20de4dbf90bda2db3732133cfd74c5a2b6538026
Aleksandra Knott <aleksandraknott@gmail.com>
2024-07-22 15:00:56 +0000

    added admin page for POST

--- codestar/settings.py
+++ codestar/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-sashacreate-djangoblog-ntwn91cfjeo.ws-eu115.gitpod.io','.herokuapp.com']
+ALLOWED_HOSTS = ['8000-sashacreate-djangoblog-2feflzpv3gx.ws-eu115.gitpod.io','.herokuapp.com']
 
 
 # Application definition
@@ -89,6 +89,10 @@ DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.codeanyapp.com",
+    "https://*.herokuapp.com"
+]
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
